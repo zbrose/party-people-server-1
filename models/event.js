02b1e702b0ae7705d405c4fa1277718113eedcb0
kokopuffz <chanheeya@gmail.com>
2022-03-29 16:02:35 -0500

    add time

--- models/event.js
+++ models/event.js
@@ -1,18 +1,19 @@
 const mongoose = require('mongoose')
 
 const EventSchema = mongoose.Schema({
-    title: String,
-    address: String,
-    city: String,
-    state: String,
-    zipcode: Number,
-    date: Date,
-    description: String,
-    category: String,
-    image: String,
-    hype: Number,
-    attendees:[{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-    host: {type: mongoose.Schema.Types.ObjectId, ref:'User'}
-  })
+  title: String,
+  address: String,
+  city: String,
+  state: String,
+  zipcode: Number,
+  time: Date,
+  date: Date,
+  description: String,
+  category: String,
+  image: String,
+  hype: Number,
+  attendees: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  host: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+})
 
 module.exports = mongoose.model('Event', EventSchema)
